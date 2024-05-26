221b7157968797c24d8995a47b78eaf99422749e
nikolay <bazhenov-nik@mail.ru>
2024-05-26 20:47:11 +0500

    one commit

---
+++ src/components/User5.vue
@@ -0,0 +1,50 @@
+<template>
+    <div className="one">
+        <button @click="deleteUser(index)">Удалить</button>
+        <h3><span>Имя пользователя: </span>{{ user.name }}</h3>
+        <p><span>Email пользователя: </span>{{user.email}}</p>
+        <p><span>Cупер секретный пароль: </span>{{user.pass}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        user:{
+            type: Object,
+            required: true
+        },
+        index:{
+            type: Number,
+            required: true
+        },
+        deleteUser:{
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.one span {
+    color: #000;
+  }
+  button{
+    border: 0;
+    border-radius: 5px;
+    outline: none;
+    padding: 10px 15px;
+    background: #d66c75;
+    font-weight: bold;
+    cursor: pointer;
+    transition: transform 500ms ease;
+    float: right;
+}
+
+button:hover{
+    transform: translateY(-5px);
+}
+
+
+</style>
